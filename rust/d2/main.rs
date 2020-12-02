fcf53ddb8923d6b021d80d1813425118ec749846
Umran Hussein <umran.hussein@gmail.com>
2020-12-02 00:42:49 -0800

    rust solutions to day 2

--- rust/d2/main.rs
+++ rust/d2/main.rs
@@ -2,12 +2,57 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-fn solution_1(_: &[String]) -> Result<i32, &str> {
-    Err("blah")
+fn parse_line(input: &str) -> Result<(usize, usize, char, Vec<char>), &str> {
+    let rule_rest: Vec<&str> = input.split(":").collect();
+    let space_pw: Vec<&str> = rule_rest.get(1).ok_or("invalid index")?.split(" ").collect();
+    let pw: Vec<char> = space_pw.get(1).ok_or("invalid index")?.chars().collect();
+    let rnge_char: Vec<&str> = rule_rest.get(0).ok_or("invalid index")?.split(" ").collect();
+    let rnge: Vec<&str> = rnge_char.get(0).ok_or("invalid index")?.split("-").collect();
+    let character_chars: Vec<char> = rnge_char.get(1).ok_or("invalid index")?.chars().collect();
+
+    let result = (
+        rnge.get(0).ok_or("invalid index")?.parse::<usize>().unwrap(),
+        rnge.get(1).ok_or("invalid index")?.parse::<usize>().unwrap(),
+        *character_chars.get(0).ok_or("invalid index")?,
+        pw
+    );
+    Ok(result)
 }
 
-fn solution_2(_: &[String]) -> Result<i32, &str> {
-    Err("blah")
+fn solution_1(input: &[String]) -> Result<i32, &str> {
+    let mut valid = 0;
+    for line in input.iter() {
+        let (min, max, character, password) = parse_line(line)?;
+        let mut count = 0;
+        for &c in password.iter() {
+            if character == c {
+                count += 1;
+            }
+        }
+
+        if count >= min && count <= max {
+            valid += 1;
+        }
+    }
+    Ok(valid)
+}
+
+fn solution_2(input: &[String]) -> Result<i32, &str> {
+    let mut valid = 0;
+    for line in input.iter() {
+        let (min, max, character, password) = parse_line(line)?;
+        let &first_char = password.get(min-1).ok_or("invalid index")?;
+        let &second_char = password.get(max-1).ok_or("invalid index")?;
+
+        if first_char == character && second_char == character {
+            continue;
+        }
+
+        if first_char == character || second_char == character {
+            valid += 1;
+        }
+    }
+    Ok(valid)
 }
 
 fn read_lines<P: AsRef<Path>>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> {
