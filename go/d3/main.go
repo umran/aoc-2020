a776e49ebcb15496c7b8b57b770142549ea7c971
Umran Hussein <umran.hussein@gmail.com>
2020-12-02 22:01:11 -0800

    day 3 solns in go

---
+++ go/d3/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func solution1(input []string, slope []int) int {
+	return treesForSlope(input, slope[0], slope[1])
+}
+
+func solution2(input []string, slopes [][]int) int {
+	answer := 1
+	for _, slope := range slopes {
+		answer = answer * treesForSlope(input, slope[0], slope[1])
+	}
+
+	return answer
+}
+
+func treesForSlope(input []string, dx, dy int) int {
+	trees := 0
+	x := 0
+	for y := 0; y < len(input); y += dy {
+		row := input[y]
+		if []rune(row)[x] == []rune("#")[0] {
+			trees++
+		}
+		x = (x + dx) % len([]rune(row))
+	}
+	return trees
+}
+
+func main() {
+	var input []string
+	file, _ := os.Open("./input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+
+	fmt.Println(solution1(input, []int{3, 1}))
+	fmt.Println(solution2(input, [][]int{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}))
+}
