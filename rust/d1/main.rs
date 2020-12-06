29a61d21a95cbe8bff3337e3909739489a206d12
Umran Hussein <umran.hussein@gmail.com>
2020-12-06 06:18:42 -0800

    use a hash set instead of a hash map where possible

--- rust/d1/main.rs
+++ rust/d1/main.rs
@@ -1,35 +1,33 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 fn solution_1(input: &[i32], sum: i32) -> Result<i32, &str> {
-    let mut compliments: HashMap<i32, i32> = HashMap::new();
-    for val in input.iter() {
+    let mut compliments: HashSet<i32> = HashSet::new();
+    for &val in input.iter() {
         let compliment = sum - val;
-        match compliments.get(&compliment) {
-            Some(_) => return Ok(compliment * val),
-            _ => (),
+        if compliments.contains(&compliment) {
+            return Ok(compliment * val);
         }
-        compliments.insert(*val, *val);
+        compliments.insert(val);
     }
 
     Err("couldn't find a solution")
 }
 
 fn solution_2(input: &[i32], sum: i32) -> Result<i32, &str> {
-    for (i, vali) in input.iter().enumerate() {
-        let mut compliments: HashMap<i32, i32> = HashMap::new();
-        for (j, valj) in input.iter().enumerate() {
+    for (i, &vali) in input.iter().enumerate() {
+        let mut compliments: HashSet<i32> = HashSet::new();
+        for (j, &valj) in input.iter().enumerate() {
             if i == j {
                 continue;
             }
             let compliment = sum - vali - valj;
-            match compliments.get(&compliment) {
-                Some(_) => return Ok(compliment * vali * valj),
-                _ => (),
+            if compliments.contains(&compliment) {
+                return Ok(compliment * vali * valj);
             }
-            compliments.insert(*valj, *valj);
+            compliments.insert(valj);
         }
     }
     
