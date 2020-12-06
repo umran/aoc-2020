cf3a69700b1d6a3b49b901b7ad444782e2a329b3
Umran Hussein <umran.hussein@gmail.com>
2020-12-06 00:00:31 -0800

    go solns day 6

---
+++ go/d6/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func solution1(groups []string) int {
+	total := 0
+	for _, group := range groups {
+		seenQuestions := make(map[rune]bool)
+		individuals := strings.Split(group, "\n")
+		for _, individual := range individuals {
+			for _, q := range individual {
+				if _, seen := seenQuestions[q]; seen {
+					continue
+				}
+				seenQuestions[q] = true
+				total++
+			}
+		}
+	}
+
+	return total
+}
+
+func solution2(groups []string) int {
+	total := 0
+	for _, group := range groups {
+		individuals := strings.Split(group, "\n")
+		answerCounts := make(map[rune]int)
+		for _, individual := range individuals {
+			for _, q := range individual {
+				answerCounts[q]++
+			}
+		}
+		for _, count := range answerCounts {
+			if count == len(individuals) {
+				total++
+			}
+		}
+	}
+
+	return total
+}
+
+func main() {
+	bytes, _ := ioutil.ReadFile("input.txt")
+	text := string(bytes)
+	groups := strings.Split(text, "\n\n")
+
+	fmt.Println(solution1(groups))
+	fmt.Println(solution2(groups))
+}
